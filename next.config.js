00bfff0c023c14ca3c29ba7bf2257baff0d4e59e
south <fancustard9@gmail.com>
2025-03-10 15:32:16 +0900

    一旦これで動くか試す

--- next.config.js
+++ next.config.js
@@ -1,7 +1,3 @@
 module.exports = {
-    reactStrictMode: true,  // 必要に応じてReactの厳格モードを有効にする
-    // turbopackを使用しない場合
-    // experimental: {
-    //   turbopack: true,  // この設定を削除またはコメントアウト
-    // },
+    // 他の設定があればそのままで
 };
